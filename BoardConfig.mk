5ad73695f0c331989e502d39318bd0b5a0179272
Saatvik Shukla <me@saatvikshukla.com>
2015-09-09 13:28:28 +0530

    Update TWRP
    Signed-off-by: Saatvik Shukla <me@saatvikshukla.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,23 +134,23 @@ BOARD_SEPOLICY_UNION += \
     property_contexts \
     service_contexts
 
-# Recovery
-TARGET_RECOVERY_FSTAB := device/sony/rhine/rootdir/fstab.rhine
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+# TWRP
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 
 # TWRP flags
-DEVICE_RESOLUTION := 1080x1920
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TW_THEME := portrait_hdpi
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
-RECOVERY_SDCARD_ON_DATA := true
 TW_HAS_NO_RECOVERY_PARTITION := true
 TW_FLASH_FROM_STORAGE := true
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
-# TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_JB_CRYPTO := true
+TW_INCLUDE_JB_CRYPTO := false
+TW_INCLUDE_L_CRYPTO := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 4095
+TW_NO_USB_STORAGE := true
 TW_CRYPTO_FS_TYPE := "ext4"
 TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/msm_sdcc.1/by-name/userdata"
 TW_CRYPTO_MNT_POINT := "/data"
@@ -158,8 +158,3 @@ TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
 TW_CRYPTO_FS_FLAGS := "0x00000406"
 TW_CRYPTO_KEY_LOC := "footer"
 TW_INCLUDE_FUSE_EXFAT := true
-# TW_BOARD_CUSTOM_GRAPHICS := ../../../device/sony/rhine/recovery/twrpgraphics.c
-TW_BRIGHTNESS_PATH := /sys/class/leds/wled:backlight/brightness
-TW_MAX_BRIGHTNESS := 4095
-TW_NO_USB_STORAGE := true
-TW_NO_SCREEN_BLANK := true
